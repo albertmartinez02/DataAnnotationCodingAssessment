869988df4d575a99882a6f0dc7bdc1f15fb98078
Albert <albertmart2002@gmail.com>
2024-08-14 08:19:13 -0400

    Updated, logic outlined

--- decode_secret_message.py
+++ decode_secret_message.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
+from bs4 import SoupStrainer
 import requests
+import re
 
 def decode(doc_url):
     #Send get request to google doc
@@ -9,9 +11,11 @@ def decode(doc_url):
     #Get max_x and max_y to determine size of grid
     max_x, max_y = 0, 0
 
-    for information in soup.find_all("span", "c1"):
-        print(information)
-    print(soup.get_text())
+    for information in soup.find_all("span"):
+        print(information.string)
+    information = soup.find_all("span")
+    print(information[0] , "This is the first info found by find all")
+
 
 if __name__ == '__main__':
     document_url = input("Enter website name: ")

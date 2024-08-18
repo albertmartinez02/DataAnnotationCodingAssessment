48f0cef29822df498b8558b1bc632744de583222
Albert <albertmart2002@gmail.com>
2024-08-17 23:02:22 -0400

    Trying to create grid

--- decode_secret_message.py
+++ decode_secret_message.py
@@ -15,18 +15,34 @@ def decode(doc_url):
     soup = BeautifulSoup(doc.text, 'html.parser', parse_only=only_table_tag)
 
     '''Next, find all numbers in the table and create the coordinates'''
-    coordinates = []
+    x_coordinates = []
+    y_coordinates = []
     characters = []
     max_x, max_y, type = 0, 0, 0
 
     for string in soup.strings:
         if re.search('[0-9][0-9]*', string):
-            coordinates.append(int(string))
-        elif len(coordinates) > 0 and len(string) == 1:
+            if(type == 0):
+                x_coordinates.append(int(string))
+                type = 1
+            else:
+                y_coordinates.append(int(string))
+                type = 0
+        elif len(x_coordinates) > 0 and len(string) == 1:
             characters.append(string)
 
-    print(coordinates)
-    print(characters)
+    #Find dimensions of grid by finding maxes(add 1 for zero index grid coordinates), and create 2d list to print the information
+    max_x = max(x_coordinates) + 1
+    max_y = max(y_coordinates) + 1
+    print(max_x, max_y)
+
+    secret_message = [None] * max_y
+    for i in range(max_y):
+        secret_message[i] = [None] * max_x
+    
+    for i in range(max_y):
+        print(secret_message[i])
+
 
 
 
